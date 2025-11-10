2d2192c75ca4756d3ef80fba5ee618dd83b71df2
shikshakunwar10 <shiksha25csu220@ncuindia.edu>
2025-11-10 09:17:53 +0530

    Create q11.c

---
+++ q11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, evenCount = 0, oddCount = 0;
+    printf("Enter number of scores: ");
+    scanf("%d", &n);
+
+    int arr[n], even[n], odd[n];
+    printf("Enter scores: ");
+    for
