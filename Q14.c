9137076193ca5c88a2e22c9d4a9e05639bd648d8
shikshakunwar10 <shiksha25csu220@ncuindia.edu>
2025-11-10 14:12:04 +0530

    Update Q14.c

--- Q14.c
+++ Q14.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int isPrime(int num) {
+    if (num < 2) return 0;
+    for (int i = 2; i <= sqrt(num); i++)
+        if (num % i == 0)
+            return 0;
+    return 1;
+}
+
+int main() {
+    int n, i, count = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (i = 0; i < n; i++)
+        if (isPrime(arr[i]))
+            count++;
+
+    printf("Number of prime numbers in the array = %d\n", count);
+    return 0;
+}
 
